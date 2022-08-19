dee0b8d6b451cab3cf2ea1ff1e52f7de955e6ab0
Harry Fitzgerald <harry.fitzgerald@truepill.com>
2022-08-19 17:06:10 +0100

    ts finally works fine

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -1,12 +1,10 @@
 local fn = vim.fn
--- Automatically install packer
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
 	PACKER_BOOTSTRAP = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
 	print("Installing packer close and reopen Neovim...")
 end
 
--- Autocommand that reloads neovim whenever you save the plugins.lua file
 vim.cmd([[
 	augroup packer_user_config
 		autocmd!
@@ -14,13 +12,11 @@ vim.cmd([[
 	augroup end
 ]])
 
--- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
 	return
 end
 
--- Have packer use a popup window
 packer.init({
 		display = {
 			open_fn = function()
@@ -30,13 +26,15 @@ packer.init({
 	}
 )
 
--- Install your plugins here
 return packer.startup(function(use)
-	-- My plugins here
-	use 'wbthomason/packer.nvim' -- Have packer manage itself
-	use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
-	use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
-	use "neovim/nvim-lspconfig"
+	use 'wbthomason/packer.nvim' 
+	use "nvim-lua/popup.nvim" 
+	use "nvim-lua/plenary.nvim" 
+	use {
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"neovim/nvim-lspconfig",
+	}
 	use "jose-elias-alvarez/null-ls.nvim"
 	use "jose-elias-alvarez/nvim-lsp-ts-utils"
 	use 'hrsh7th/cmp-nvim-lsp'
@@ -49,31 +47,30 @@ return packer.startup(function(use)
 	use 'L3MON4D3/LuaSnip'
 	use 'simrat39/rust-tools.nvim'
 	use 'TimUntersberger/neogit'
+	use 'jxnblk/vim-mdx-js'
 	use 'jparise/vim-graphql'
+	use 'mfussenegger/nvim-dap'
 	use { 'sindrets/diffview.nvim', requires = 'nvim-lua/plenary.nvim' }
 	use { 'nvim-telescope/telescope.nvim', tag = '0.1.0' }
 	use {
-	    'kyazdani42/nvim-tree.lua',
-	    requires = {
-		    'kyazdani42/nvim-web-devicons', -- optional, for file icons
-	    },
-	    tag = 'nightly' -- optional, updated every week. (see issue #1193)
+		'kyazdani42/nvim-tree.lua',
+		requires = {
+			'kyazdani42/nvim-web-devicons', 
+		}
 	}
 	use {
-        'nvim-treesitter/nvim-treesitter',
-        run = ':TSUpdate'
-    }
+		'nvim-treesitter/nvim-treesitter',
+		run = ':TSUpdate'
+	}
 
-	
-
-	-- Automatically set up your configuration after cloning packer.nvim
-	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
 		require('packer').sync()
 	end
 
 	require("nvim-tree").setup()
+	require("mason").setup()
 
 end)
+
 
 
--- nvim/lua/user/ts-config.lua
+++ nvim/lua/user/ts-config.lua
@@ -53,9 +53,12 @@ lspconfig.tsserver.setup({
 
 null_ls.setup({
     sources = {
-		null_ls.builtins.code_actions.eslint_d,
-		null_ls.builtins.diagnostics.eslint_d,
-		null_ls.builtins.formatting.eslint_d,
+		null_ls.builtins.code_actions.eslint,
+		--null_ls.builtins.diagnostics.eslint,
+		null_ls.builtins.formatting.eslint,
+		--null_ls.builtins.diagnostics.tsc,
+		null_ls.builtins.formatting.rustfmt,
+		null_ls.builtins.formatting.rustywind,
     },
     on_attach = on_attach,
 })
