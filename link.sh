5dc68d266e9b552b68054387b3f45943739025b1
Harry Fitzgerald <harry.fitzgerald@globalfreightsolutions.com>
2025-05-01 09:13:46 +0100

    feat: add sess and sess-mini scripts for tmux session management

--- link.sh
+++ link.sh
@@ -11,3 +11,9 @@ ln -s $(pwd)/shell/zshrc ~/.zshrc
 ln -s $(pwd)/shell/telemetry.sh ~/.telemetry.sh
 
 ln -s $(pwd)/shell/rg.conf ~/.rg.conf
+
+# WSL doesn't like symlinks here for some reason?
+echo "Initialising scripts"
+for file in bin/*; do
+	cp $file ~/.bin/$(basename "$file")
+done
